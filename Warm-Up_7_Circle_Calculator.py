e14e36586ddecaa10316146248e8a60f95cc289b
yader012 <yader114946@mcpsva.org>
2025-12-04 10:01:45 -0500

    Add files via upload

---
+++ Warm-Up_7_Circle_Calculator.py
@@ -0,0 +1,23 @@
+"""
+Filename: Warm-Up_7_Circle_Calculator.py
+Author: <Lastname, Firstname>
+Created: <MM/DD/YYYY>
+Instructor: Holtslander
+"""
+
+def circle_calculator():
+    """
+    Calculates the area and circumference of a circle.
+    Asks the user to enter the radius of the circle.
+    Uses the formulas
+    A = pi*r**2
+    C = 2*pi*r
+    :return: None
+    """
+    ### YOUR CODE GOES HERE ###
+    
+
+
+### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
+if __name__ == '__main__':
+    circle_calculator()
